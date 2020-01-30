2eb6d884b43595c85cc3e761a9f58aa456964131
Ethan Hocking <eshocking253@gmail.com>
2020-01-30 14:22:56 -0700

    Add interaction pattern for reference

--- test/event_test.rb
+++ test/event_test.rb
@@ -5,3 +5,28 @@ require './lib/event'
 class EventTest < Minitest::Test
 
 end
+
+# pry(main)> require './lib/event'
+# #=> true
+#
+# pry(main)> event = Event.new("Curling", [24, 30, 18, 20, 41])
+# #=> #<Event:0x00007fba3fc42ab0 @ages=[24, 30, 18, 20, 41], @name="Curling">
+#
+# pry(main)> event.name
+# #=> "Curling"
+#
+# pry(main)> event.ages
+# #=> [24, 30, 18, 20, 41]
+#
+# pry(main)> event.max_age
+# #=> 41
+#
+# pry(main)> event.min_age
+# #=> 18
+#
+# pry(main)> event.average_age
+# #=> 26.6
+#
+# pry(main)> event.standard_deviation_age
+# #=> 8.28
+#
